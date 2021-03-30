be4bc6a3752c035c11e6767f6a7869bd59a7befd
Jenni Liljakko <jenni.liljakko@helsinki.fi>
2021-03-30 19:24:45 +0300

    toinen laskaritehtävä valmis

--- laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
+++ laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
@@ -7,3 +7,19 @@ class TestMaksukortti(unittest.TestCase):
 
     def test_luotu_kortti_on_olemassa(self):
         self.assertNotEqual(self.maksukortti, None)
+
+    def test_kortin_saldo_alussa_oikein(self):
+        self.assertEqual("saldo: 0.1", str(self.maksukortti))
+
+    def test_rahan_lataaminen_kasvattaa_saldoa_oikein(self):
+        self.maksukortti.lataa_rahaa(10)
+        self.assertEqual("saldo: 0.2", str(self.maksukortti))
+
+    def test_rahan_ottaminen_toimii_oikein(self):
+        self.assertTrue(self.maksukortti.ota_rahaa(5))
+        self.assertEqual("saldo: 0.05", str(self.maksukortti))
+    
+    def test_saldo_ei_vahene_jos_rahaa_ei_ole_tarpeeksi(self):
+        self.assertFalse(self.maksukortti.ota_rahaa(15))
+        self.assertEqual("saldo: 0.1", str(self.maksukortti))
+        
